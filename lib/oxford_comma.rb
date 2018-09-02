859bfc0dced3c8c417b2c360f83288ff0f894c02
Steven Zapata <szapata427@gmail.com>
2018-09-02 23:52:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
-def oxford_comma(array)
-if array.size == 0 || array.size == 1
-	    array.join
-  elsif array.size == 2
-    array[array.size - 1] = " and "+array[array.size - 1]
-    array.join
-  else
-    array[array.size - 1] = "and "+array[array.size - 1]
-    array.join(", ")
-  end
-end
+# def oxford_comma(array)
+# if array.size == 0 || array.size == 1
+# 	    array.join
+#   elsif array.size == 2
+#     array[array.size - 1] = " and "+array[array.size - 1]
+#     array.join
+#   else
+#     array[array.size - 1] = "and "+array[array.size - 1]
+#     array.join(", ")
+#   end
+# end
 
 def oxford_comma(array)
   
