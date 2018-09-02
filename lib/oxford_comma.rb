19bc769a4caf1b55423b92cfc1ab924a4e5f543f
Steven Zapata <szapata427@gmail.com>
2018-09-02 23:56:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,24 +1,3 @@
-# def oxford_comma(array)
-# if array.size == 0 || array.size == 1
-# 	    array.join
-#   elsif array.size == 2
-#     array[array.size - 1] = " and "+array[array.size - 1]
-#     array.join
-#   else
-#     array[array.size - 1] = "and "+array[array.size - 1]
-#     array.join(", ")
-#   end
-# end
+
 
 def oxford_comma(array)
-  
-  if array.length < 3
-    str = array.join(" and ")
-  else
-    lastval = array.pop
-    array << "and "
-    str = array.join(", ")
-    str << lastval
-  end
-  str
-end
