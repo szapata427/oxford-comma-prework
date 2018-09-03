5a35ef33ddb251d03b7bd3e58e28e2fe6696074c
Steven Zapata <szapata427@gmail.com>
2018-09-03 00:00:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,4 +5,10 @@ def oxford_comma(array)
   if array.length < 3
   str = array.join(" and ")
   else
-    
+    last = array.pop
+    array.push("and")
+    array.join(", ")
+    array.push(last)
+  end
+  return str
+end
