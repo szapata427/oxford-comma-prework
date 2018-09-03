2fd0b8563337254d4f35ebcc7d06b36e337fc603
Steven Zapata <szapata427@gmail.com>
2018-09-03 00:02:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  
+  if array.length < 3
+  str = array.join(" and ")
+  
+  else
+    last = array.pop
+    array.push("and ")
+   str = array.join(", ")
+    str << last
+  end
+   str
 end
